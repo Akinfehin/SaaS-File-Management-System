bf4f98c43e4732b4557b32000a2d3a8f023a3ca5
Akinfehin <akinfehin@gmail.com>
2023-04-14 20:10:05 +0100

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -38,12 +38,12 @@ if (isset($_POST['submit'])) {
                 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
                 $mail->Username   = 'akinfehin@gmail.com';                     //SMTP username
-                $mail->Password   = 'rmnvqoshdvsnacpe';                               //SMTP password
+                $mail->Password   = '';                               //SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                 //Recipients
-               // $mail->setFrom('toledojonel557@gmail.com');
+               // $mail->setFrom(');
                 $mail->setFrom($email);
                 $mail->addAddress($email);
 
